9cfd3cc974266cbca49e889333b645685b0e569f
yuanshenmick <yuanshenmick@gmail.com>
2019-05-07 17:03:29 +0800

    938

---
+++ leetcode/938.RangeSumofBST.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+function rangeSumBST(root, L, R) {
+  if (!root) return 0;
+
+  let sum = 0;
+  if (root.val >= L && root.val <= R) {
+    sum += root.val;
+  }
+  sum += rangeSumBST(root.left, L, R);
+  sum += rangeSumBST(root.right, L, R);
+  return sum;
+}
