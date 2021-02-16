fa9a27d6b26644f1ca2c9234818e4e10f6413057
mickmetalholic <yuanshenmick@gmail.com>
2021-02-16 15:43:33 +0800

    561

---
+++ leetcode-cn/561-array-partition-i.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function arrayPairSum(nums) {
+  nums.sort((a, b) => a - b);
+  let res = 0;
+  for (let i = 0; i < nums.length; i += 2) {
+    res += nums[i];
+  }
+  return res;
+}
