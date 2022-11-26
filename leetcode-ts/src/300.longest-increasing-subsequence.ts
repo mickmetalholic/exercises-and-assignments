bf438a580fdc0282735fda9568e9ec74c9c8d226
mickmetalholic <yuanshenmick@gmail.com>
2022-11-26 17:47:40 +0800

    300

--- leetcode-ts/src/300.longest-increasing-subsequence.ts
+++ leetcode-ts/src/300.longest-increasing-subsequence.ts
@@ -6,6 +6,21 @@
 
 // @lc code=start
 function lengthOfLIS (nums: number[]): number {
+  const lengths = [1]
+  let longest = 1
 
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i]
+    let length = 1
+    for (let j = 0; j < i; j++) {
+      if (num > nums[j]) {
+        length = Math.max(length, lengths[j] + 1)
+      }
+    }
+    lengths[i] = length
+    longest = Math.max(longest, length)
+  }
+
+  return longest
 }
 // @lc code=end
