36b78eb2ccae6fc80a03ba4465f983d9da4c939d
mickmetalholic <yuanshenmick@gmail.com>
2023-01-12 19:57:15 +0800

    Create 877.stone-game.ts

---
+++ leetcode-ts/src/877.stone-game.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=877 lang=typescript
+ *
+ * [877] Stone Game
+ */
+
+// @lc code=start
+function stoneGame (piles: number[]): boolean {
+  return true
+}
+// @lc code=end
