558466826655b0886de995e8b2ae0e18aeccd0a4
Shen Yuan <yuanshenmick@gmail.com>
2023-03-12 15:03:12 +0800

    Update 486.predict-the-winner.ts

--- leetcode-ts/src/486.predict-the-winner.ts
+++ leetcode-ts/src/486.predict-the-winner.ts
@@ -16,10 +16,6 @@ function PredictTheWinner (nums: number[]): boolean {
     for (let j = i; j < nums.length; j++) {
       if (i === j) {
         dp[i][j] = nums[i]
-      } else if (i === nums.length - 1) {
-        dp[i][j] = nums[i] - dp[i + 1][j]
-      } else if (j === 0) {
-        dp[i][j] = nums[j] - dp[i][j - 1]
       } else {
         dp[i][j] = Math.max(
           nums[i] - dp[i + 1][j],
