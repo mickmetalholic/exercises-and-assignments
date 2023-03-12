763f408fd0951354c3c966e46a7c93e7a8435166
Shen Yuan <yuanshenmick@gmail.com>
2023-03-12 16:43:04 +0800

    Create 23.merge-k-sorted-lists.ts

---
+++ leetcode-ts/src/23.merge-k-sorted-lists.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=23 lang=typescript
+ *
+ * [23] Merge k Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class Heap {
+
+}
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+  const list = new ListNode()
+
+  let node = list
+}
+// @lc code=end
+
